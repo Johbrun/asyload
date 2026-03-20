825232e872f5c6657a2fd1cce0315e2267f7a516
Johan <31695713+Johbrun@users.noreply.github.com>
2026-03-20 22:09:44 +0100

    Add console logs for debugging in hell.js

--- hell.js
+++ hell.js
@@ -1,7 +1,11 @@
+console.log(1);
 var f = document.createElement('iframe');
+console.log(2);
 f.style.display = 'none';
+console.log(3);
 
 var links = document.querySelectorAll('a');
+console.log(links);
 for (var i = 0; i < links.length; i++) {
   if (links[i].textContent.includes('Mon Profil')) {
     f.src = links[i].href;
